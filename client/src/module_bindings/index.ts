b62cc76d78e649b527b636bfb50e8ccf7753403b
OohBen <89159600+OohBen@users.noreply.github.com>
2026-06-06 02:42:44 -0400

    P1E4: add turn phase state machine

--- client/src/module_bindings/index.ts
+++ client/src/module_bindings/index.ts
@@ -34,6 +34,10 @@ import {
 } from "spacetimedb";
 
 // Import all reducer arg schemas
+import AdvanceTurnPhaseReducer from "./advance_turn_phase_reducer";
+import AdvanceWorldReducer from "./advance_world_reducer";
+import CreateSessionReducer from "./create_session_reducer";
+import JoinOrResumeSessionReducer from "./join_or_resume_session_reducer";
 
 // Import all procedure arg schemas
 
@@ -253,6 +257,10 @@ const tablesSchema = __schema({
 
 /** The schema information for all reducers in this module. This is defined the same way as the reducers would have been defined in the server, except the body of the reducer is omitted in code generation. */
 const reducersSchema = __reducers(
+  __reducerSchema("advance_turn_phase", AdvanceTurnPhaseReducer),
+  __reducerSchema("advance_world", AdvanceWorldReducer),
+  __reducerSchema("create_session", CreateSessionReducer),
+  __reducerSchema("join_or_resume_session", JoinOrResumeSessionReducer),
 );
 
 /** The schema information for all procedures in this module. This is defined the same way as the procedures would have been defined in the server. */
